fbaa7a76e366962b78900cfd3417e8fa1ed09074
Komal Tanveer <sweetykomal1998@gmail.com>
2024-03-08 02:12:36 +0500

    first commit

---
+++ 3.Name Cases/main.ts
@@ -0,0 +1,7 @@
+//Question # 3
+//Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
+let myName: string ="Komal Tanveer";
+console.log(myName.toLowerCase());
+console.log(myName.toUpperCase());
+console.log(myName.charAt(0).toUpperCase() + myName.slice(1).toLowerCase());
