7eab81430f4c98a389bcf42fb62594271e1dcc1d
Komal Tanveer <sweetykomal1998@gmail.com>
2024-03-09 16:58:09 +0500

    first commit

---
+++ 9. Favourite Number/main.js
@@ -0,0 +1,4 @@
+/*Question # 9
+Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message*/
+var favoriteNumber = 19;
+console.log("My Favourite number is ".concat(favoriteNumber));
