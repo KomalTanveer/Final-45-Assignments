8aafb68c983bd2752e8bc8d94b1a18353a9608fd
Komal Tanveer <sweetykomal1998@gmail.com>
2024-03-09 16:51:46 +0500

    first commit

---
+++ 5.Famous Quotes 2/main.ts
@@ -0,0 +1,5 @@
+//Question # 5
+//Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+
+let famousPerson: string ="Christian D. Larson";
+console.log(`${famousPerson} once said, "Believe in yourself and all that you are. Know that there is something inside you that is greater than any obstacle."`);
